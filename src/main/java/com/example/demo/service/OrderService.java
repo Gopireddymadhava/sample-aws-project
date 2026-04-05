d080e7b4494c55ac4fbfd4f06083cf9e84fed2c0
Gopireddy-123 <161678256+Gopireddy-123@users.noreply.github.com>
2026-04-05 20:38:27 +0530

    Intial commit

---
+++ src/main/java/com/example/demo/service/OrderService.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Order;
+import com.example.demo.repository.OrderRepository;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public Order addOrder(Order order){
+       return repository.save(order);
+    }
+
+    public List<Order> getOrders(){
+        return repository.findAll();
+    }
+
+    public Order getOrderById(int id){
+        return repository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("Invalid id : "+id));
+    }
+}
